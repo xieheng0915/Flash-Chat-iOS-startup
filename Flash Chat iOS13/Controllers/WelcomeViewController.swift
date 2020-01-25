fc50c3a0a8aa941a297297005d2ef072b3382fb5
xieheng <xieheng0915@gmail.com>
2020-01-25 23:55:45 +0900

    initial commitment

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,16 +7,33 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
+        titleLabel.text = K.appName
+        
+// --These are original codes without using CLTypingLabeln --
+        
+//        var charIndex = 0.0
+//        titleLabel.text = ""
+//        let titleText = "⚡️FlashChat"
+//
+//
+//        for letter in titleText {
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charIndex += 1
+//        }
+         
     }
+        
     
 
 }
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Xieheng on 2020/01/23.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
